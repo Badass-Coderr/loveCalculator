bfac84cba4528a3742275aa0e9052e653a387373
Anshuman Mahato <AnshumanMahato@users.noreply.github.com>
2020-08-22 11:32:50 +0530

    Server
    Check readme for changelog...

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Love % calculator on the basis of couples names
 
 For now we have just made it for mobiles, so just compress the browser window into a mobile view.
+
+
+##Lc-v- percentage update
+- a temporary score page added. File name percent.hbs
+- Form working added,
+- node server
+- html pages have been changed to .hbs(handlebars) for templating purposes
+- file organization modified:
+  - All static client side files(css, client side js, svg, etc) are in the "public" folder.
+  - Server resources are in the "server" folder.
+  - Templates are in "templates" folder.
+
+
+###Info:
+server port : 3000
+**Note: As it is in testing phase, failing condition have not been added for the result.** 
+
---
+++ server/server.js
@@ -0,0 +1,65 @@
+// modules
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+require("./js_modules/percent_lc.js")();
+
+// paths
+
+const publicDirectory = path.join(__dirname,"/../public");
+const viewsDirectory = path.join(__dirname,"/../templates/views");
+const partialsDirectory = path.join(__dirname,"/../templates/partials");
+
+// express setup
+
+const app = express();
+app.use(express.static(publicDirectory));
+
+// hbs setup
+
+app.set('view engine','hbs');
+app.set('views',viewsDirectory)
+hbs.registerPartials(partialsDirectory);
+
+//bodyParser setup
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//File reading
+let rawData = fs.readFileSync("./description.json");
+let description = JSON.parse(rawData);
+console.log(description);
+
+// Routes
+
+app.get('/',(req,res) => {
+    res.render("index",{
+        title : "Calculate the percentage"
+    });
+});
+
+app.post('/',(req,res) => {
+    console.log(req.body);
+    let name1 = req.body.name_1,
+        name2 = req.body.name_2;
+    let result = percent(name1,name2);
+    let resultDesc;
+    if(result < 25)
+        resultDesc = description[0].desc;
+    else if(result <50)
+        resultDesc = description[1].desc;
+    else if(result <75)
+        resultDesc = description[2].desc;
+    else
+        resultDesc = description[3].desc;
+    res.render("percent",{
+        title : "Score",
+        score : result + "%",
+        desc : resultDesc
+    });
+});
+
+app.listen(3000);
